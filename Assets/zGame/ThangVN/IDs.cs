a34f02bbe8302ececa847729915918639e963553
ThangVN <95681827+VuNgocThang@users.noreply.github.com>
2024-12-04 16:26:09 +0700

    update ID

--- Assets/zGame/ThangVN/IDs.cs
+++ Assets/zGame/ThangVN/IDs.cs
@@ -25,6 +25,36 @@ namespace ThangVN
 		public const int Level_18 = 17;
 		public const int Level_19 = 18;
 		public const int Level_20 = 19;
+		public const int Level_21 = 20;
+		public const int Level_22 = 21;
+		public const int Level_23 = 22;
+		public const int Level_24 = 23;
+		public const int Level_25 = 24;
+		public const int Level_26 = 25;
+		public const int Level_27 = 26;
+		public const int Level_28 = 27;
+		public const int Level_29 = 28;
+		public const int Level_30 = 29;
+		public const int Level_31 = 30;
+		public const int Level_32 = 31;
+		public const int Level_33 = 32;
+		public const int Level_34 = 33;
+		public const int Level_35 = 34;
+		public const int Level_36 = 35;
+		public const int Level_37 = 36;
+		public const int Level_38 = 37;
+		public const int Level_39 = 38;
+		public const int Level_40 = 39;
+		public const int Level_41 = 40;
+		public const int Level_42 = 41;
+		public const int Level_43 = 42;
+		public const int Level_44 = 43;
+		public const int Level_45 = 44;
+		public const int Level_46 = 45;
+		public const int Level_47 = 46;
+		public const int Level_48 = 47;
+		public const int Level_49 = 48;
+		public const int Level_50 = 49;
 		#endregion
 	}
 }
