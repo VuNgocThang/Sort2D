3f3f88120c7bb66615ae2a0ce341bffada6a1bb2
ThangVN <95681827+VuNgocThang@users.noreply.github.com>
2025-03-18 14:43:39 +0700

    delete Airbridge

--- Assets/Plugins/Airbridge/Android/java/co/ab180/airbridge/unity/AirbridgeCallback.java
+++
@@ -1,6 +0,0 @@
-package co.ab180.airbridge.unity;
-
-public interface AirbridgeCallback {
-
-    void Invoke(String arg);
-}
--- Assets/Plugins/Airbridge/Android/java/co/ab180/airbridge/unity/AirbridgeCallbackWithReturn.java
+++
@@ -1,6 +0,0 @@
-package co.ab180.airbridge.unity;
-
-public interface AirbridgeCallbackWithReturn {
-
-    String Invoke(String arg);
-}
